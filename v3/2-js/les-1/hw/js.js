c87c38d67a1f1b1441e167e5e2f7e599fc1d0b8d
l8kout <sqet123@gmail.com>
2022-07-22 01:17:13 +0300

    hello

---
+++ v3/2-js/les-1/hw/js.js
@@ -0,0 +1,96 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let a = 'hello';
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b = 'owu';
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c = 'com';
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 'ua';
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// let e = 1;
+// console.log(e);
+// alert(e);
+// document.write(e);
+//
+// let f = 10;
+// console.log(f);
+// alert(f);
+// document.write(f);
+//
+// let g = -999;
+// console.log(g);
+// alert(g);
+// document.write(g);
+//
+// let h = 123;
+// console.log(h);
+// alert(h);
+// document.write(h);
+//
+// let i = 3.14;
+// console.log(i);
+// alert(i);
+// document.write(i);
+//
+// let j = 2.7;
+// console.log(j);
+// alert(j);
+// document.write(j);
+//
+// let k = 16;
+// console.log(k);
+// alert(k);
+// document.write(k);
+//
+// let x = true;
+// console.log(x);
+// alert(x);
+// document.write(x);
+//
+// let z = false;
+// console.log(z);
+// alert(z);
+// document.write(z);
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// let firstName = 'Мельник';
+// let middleName = 'Юрій';
+// let lastName = 'Олексанрович';
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(person);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+
+//   let a = 100;
+//   let b = '100';
+//   let c = true;
+//
+//   console.log(typeof a + ' ' + a);
+//   console.log(typeof b + ' ' + b);
+//   console.log(typeof c + ' ' + c);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+    let aa = prompt('Введіть своє імʼя');
+    let bb = prompt('Введіть своє прізвище');
+    let cc = prompt('Введіть своє іʼмя по батькові');
+
+    console.log(aa + ' ' + bb + ' ' + cc);
+
+
